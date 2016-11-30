baf145e9da050ad094e9923ba245709913235a0f
pachimorfo <carsan15@ucm.es>
2016-11-30 12:39:39 +0100

    Ejercicio 7 terminado, falta algo del 6

--- js/main.js
+++ js/main.js
@@ -39,15 +39,18 @@ battle.on('turn', function (data) {
     console.log('TURN', data);
 
     // TODO: render the characters****
+    //Para sacar los nombres de los personajes para usarlos como ID.
     var characterIDlist = Object.keys(this._charactersById);
     var list = document.querySelectorAll('.character-list');
     var heroesList = list[0];
     var monstersList = list[1];
-    var render;
+    var render = " ";
     var charct;
+    heroesList.innerHTML = " ";
+    monstersList.innerHTML = " ";
 	for (var i = 0; i < characterIDlist.length; i++){
  	 	charct = this._charactersById[characterIDlist[i]];
- 	 	render = '<li data-chara- id="'+characterIDlist[i]+'">'+charct.name+
+ 	 	render = '<li data-chara-id="'+characterIDlist[i]+'">'+charct.name+
  	 	'(HP: <strong>'+charct.hp+'</strong>/'+charct.maxHp+
  	 	',MP: <strong>'+charct.mp+'</strong>/'+charct.maxMp+') </li>';
  	 	if(charct.party === 'heroes')
@@ -56,7 +59,7 @@ battle.on('turn', function (data) {
  	 		monstersList.innerHTML += render;
  	}
   	// TODO: highlight current character****
-  	var charActive = document.querySelector("#"+data.activeCharacterId);
+  	var charActive = document.querySelector('[data-chara-id="' + data.activeCharacterId + '"]');
   	charActive.classList.add('active');
     // TODO: show battle actions form
     actionForm.style.display = 'inline';
@@ -64,21 +67,42 @@ battle.on('turn', function (data) {
     //de actionForm.
     var options = battle.options.list();
     var renderOpt;
+    choices[0].innerHTML = " ";
    	for(var i = 0; i < options.length; i++){
     	renderOpt = '<li><label><input type="radio" name="option" value="' +options[i]+
     	'"> ' + options[i] + '</label></li>';
     	choices[0].innerHTML += renderOpt;
  	}
-
-
-
-
 });
 
 battle.on('info', function (data) {
     console.log('INFO', data);
 
     // TODO: display turn info in the #battle-info panel
+    var activeChar = data.activeCharacterId;
+    var objective = data.targetId;
+   
+    infoPanel.innerHTML = "FIGHT! Good Luck";
+    if (data.action ==='attack'){
+    	var effectsTxt = prettifyEffect(data.effect || {});
+    	infoPanel.innerHTML =`<strong>${activeChar}</strong> attacked <strong> ${objective}</strong>${data.success ? '. Dealing ' : '. But failed.'} ${data.success ? effectsTxt : ''}`;
+	}
+	else if(data.action === 'defend'){//Hubieramos usado la sintaxis con las comillas normales, '', y con los + para
+		//concaternar strings, pero no sabemos por que, no nos iba correctamente, asi que al final, un compañero nos recomendo usar las de ` y el 
+		//y de esta forma todo va correctamente.
+		infoPanel.innerHTML =`<strong>${activeChar}</strong> defended <strong>
+		${data.success ? 'successfully.' : 'and failed. ' }</strong> Defense = ${data.newDefense} . Shields ACTIVATED`;
+		
+	}
+	else{
+		 var effectsTxt = prettifyEffect(data.effect || {});
+		 var spell = data.scrollName;
+		 infoPanel.innerHTML = `<strong>${activeChar}</strong> casted ${spell} on <strong>${objective}</strong>${data.success ? ' and caused ' : '. But failed.'} ${data.success ? effectsTxt : ''}`;
+   	}
+
+
+
+
 });
 
 battle.on('end', function (data) {
@@ -112,41 +136,103 @@ window.onload = function () {
         evt.preventDefault();
 
         // TODO: select the action chosen by the player
+        var action = actionForm.elements['option'].value;
+        //action.innerhtml += '<input type="radio" name="option" value="' + action + '" required>'
+        battle.options.select(action);
         // TODO: hide this menu
+        actionForm.style.display = 'none';
         // TODO: go to either select target menu, or to the select spell menu
+        if (action === 'defend'){
+          actionForm.style.display = 'inline';
+        }
+        else if(action === 'attack'){
+        	var targets = targetForm.getElementsByClassName("choices");//Busca la clase con el nombre 
+        	//indicado dentro de actionForm.
+    		var options = battle.options.list();
+    		var renderT;
+    		targets[0].innerHTML = " ";
+   			for(var i = 0; i < options.length; i++){
+    			renderT = '<li><label><input type="radio" name="option" value="' +options[i]+
+    			'"> ' + options[i] + '</label></li>';
+    			targets[0].innerHTML += renderT;
+ 			}
+        	targetForm.style.display = 'inline';        	
+        }
+        else{//*******
+        	var spells = spellForm.getElementsByClassName("choices");//Busca la clase con el nombre 
+        	//indicado dentro de actionForm.
+        	var activeParty = battle._activeCharacter.party;
+    		var options = battle._grimoires[activeParty];
+    		var renderS;
+    		spells[0].innerHTML = " ";
+   			for(var i in options){
+    			renderS = '<li><label><input type="radio" name="option" value="' +options[i].name+
+    			'"> ' + options[i].name+ '</label></li>';
+    			spells[0].innerHTML += renderS;
+ 			}
+        	spellForm.style.display = 'inline';
+        	
+        }
+
     });
 
     targetForm.addEventListener('submit', function (evt) {
         evt.preventDefault();
         // TODO: select the target chosen by the player
+        var target = targetForm.elements['option'].value;
+        battle.options.select(target);
         // TODO: hide this menu
+        targetForm.style.display = 'none';
+        actionForm.style.display = 'block';      
+
     });
 
     targetForm.querySelector('.cancel')
     .addEventListener('click', function (evt) {
         evt.preventDefault();
-        /* for cada monstruo
-        characterlist.innerhtml
-
-        */
         // TODO: cancel current battle options
+        battle.options.cancel();
         // TODO: hide this form
+        targetForm.style.display = 'none';
         // TODO: go to select action menu
+        actionForm.style.display = 'block';
     });
 
     spellForm.addEventListener('submit', function (evt) {
         evt.preventDefault();
         // TODO: select the spell chosen by the player
+        var spell =  spellForm.elements['option'].value;
+        battle.options.select(spell);
         // TODO: hide this menu
+        spellForm.style.display = 'none';
         // TODO: go to select target menu
+		//Rellenar los targets, por que si intentas castear antes de haber ataco, que es 
+		//cuando realmente rellenas los targets, no te muestra ninguno, asi que por si acaso
+		//lo hacemos aqui tambien.       
+        
+		//SI NO HAY MANA, PONER BUTTON DISABLED
+        var targets = targetForm.getElementsByClassName("choices");//Busca la clase con el nombre 
+        	//indicado dentro de actionForm.
+    	var options = battle.options.list();
+    	var renderT;
+    	targets[0].innerHTML = " ";
+   		for(var i = 0; i < options.length; i++){
+    		renderT = '<li><label><input type="radio" name="option" value="' +options[i]+
+    		'"> ' + options[i] + '</label></li>';
+    		targets[0].innerHTML += renderT;
+ 		}
+        targetForm.style.display = 'inline'; 
     });
 
     spellForm.querySelector('.cancel')
     .addEventListener('click', function (evt) {
         evt.preventDefault();
         // TODO: cancel current battle options
+        battle.options.cancel();
         // TODO: hide this form
+        spellForm.style.display = 'none';
         // TODO: go to select action menu
+        actionForm.style.display = 'block';
     });
 
     battle.start();
